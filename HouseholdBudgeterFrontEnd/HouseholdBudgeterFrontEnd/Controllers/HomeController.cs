43a14a8769943183f819bf1bc597d6a778676ca4
Nour Mohammed <noureldinkhedr85@gmail.com>
2019-05-30 14:56:53 -0500

    3rd commit

--- HouseholdBudgeterFrontEnd/HouseholdBudgeterFrontEnd/Controllers/HomeController.cs
+++ HouseholdBudgeterFrontEnd/HouseholdBudgeterFrontEnd/Controllers/HomeController.cs
@@ -57,16 +57,84 @@ namespace HouseholdBudgeterFrontEnd.Controllers
 
             var response = httpClient.PostAsync(url, encodedValues).Result;
 
-            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
-                //Read the response
                 var data = response.Content.ReadAsStringAsync().Result;
 
-                //Convert the data back into an object
                 var result = JsonConvert.DeserializeObject<RegisterNewUserBindingModel>(data);
+
+                
+
+                return RedirectToAction(nameof(HomeController.Index));
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var data = response.Content.ReadAsStringAsync().Result;
+                var result = JsonConvert.DeserializeObject<APIModelError>(data);
+                ViewBag.ErrorMessage = result;
+                foreach (var state in result.ModelState)
+                {
+                    foreach (var x in state.Value)
+                    {
+                        ModelState.AddModelError("", x);
+                    }
+                }
+
+                return View();
+            }
+            else if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+            {
+                return View("~/Views/Shared/Error.cshtml");
             }
 
+            return RedirectToAction(nameof(HomeController.Index));
+        }
 
+        public ActionResult InternalServiceError()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Login(LoginBindingModel model)
+        {
+            var url = "http://localhost:55669/token";
+
+            var userName = model.Email;
+            var password = model.Password;
+            var grantType = "password";
+
+            var httpClient = new HttpClient();
+
+            var parameters = new List<KeyValuePair<string, string>>();
+            parameters.Add(new KeyValuePair<string, string>("username", userName));
+            parameters.Add(new KeyValuePair<string, string>("password", password));
+            parameters.Add(new KeyValuePair<string, string>("grant_type", grantType));
+
+            var encodedValues = new FormUrlEncodedContent(parameters);
+
+            var response = httpClient.PostAsync(url, encodedValues).Result;
+
+            var data = response.Content.ReadAsStringAsync().Result;
+
+            var result = JsonConvert.DeserializeObject<LoginData>(data);
+
+            // Session["Token"] = result.AccessToken;
+
+            var cookie = new HttpCookie("MyFavoriteCookie",
+                result.AccessToken);
+
+            Response.Cookies.Add(cookie);
 
             return View("Index");
         }
--- HouseholdBudgeterFrontEnd/HouseholdBudgeterFrontEnd/Models/LoginBindingModel.cs
+++ HouseholdBudgeterFrontEnd/HouseholdBudgeterFrontEnd/Models/LoginBindingModel.cs
@@ -6,23 +6,16 @@ using System.Web;
 
 namespace HouseholdBudgeterFrontEnd.Models
 {
-    public class RegisterNewUserBindingModel
+    public class LoginBindingModel
     {
         [Required]
-        [EmailAddress]
         [Display(Name = "Email")]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm Password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
-
     }
 }
